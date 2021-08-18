f76f00021c4626c517dbc08a1cc82dbd83109208
xxakatukixx <xxakatuki0412xx@gmail.com>
2021-08-18 23:14:09 +0900

    First Commit
    インストール手順書（覚書）及びHomebrew用インストールシェル

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# 自分用Mac用Dotfile
+- 自分用のMacのDotfile。
+- 個人的に必要な物の寄せ集め。
+- 個人用なので設定ミス等含まれる可能性あり。使用する場合は注意。
+
+# Homebrewインストール
+- 何が無くともここから始めないと何も出来ない。
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
+
+```
+cd
+git clone https://github.com/xxakatukixx/dotfiles.git
+cd ~/dotfiles
+./homebrew.sh
+```
+
+以下タスク。工事中。
+# zshの設定を考える
+
+# GUI側の設定
---
+++ homebrew.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# DESCRIPTION
+# Installs Homebrew software.
+# Modan CLI Tools
+brew install ripgrep
+brew install bat
+brew install exa
+brew install procs
+brew install pstree
+brew install colordiff
+# De Facto Standard
+brew install gnu-tar
+brew install curl
+brew install vim
+# Other
+brew install tmux
+
+# Google
+brew install --cask google-drive
+brew install --cask google-chrome
+# Editor
+brew install --cask obsidian
+brew install --cask visual-studio-code
+brew install --cask notion
+# other
+brew install --cask iterm2
+brew install --cask alfred
+brew install --cask clipy
