b039e7e4d50d36f126f8d7f9f0666513914f2671
xxakatukixx <xxakatuki0412xx@gmail.com>
2022-08-28 10:28:25 +0900

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -6,6 +6,7 @@
 brew install ripgrep
 brew install bat
 brew install exa
+brew install lsd
 brew install procs
 brew install pstree
 brew install colordiff
@@ -15,16 +16,30 @@ brew install curl
 brew install vim
 # Other
 brew install tmux
-brew install peco
+brew install fzf
+brew install mas
+brew tap homebrew/cask-fonts
+brew install font-hackgen
+brew install font-hackgen-nerd
+brew install romkatv/powerlevel10k/powerlevel10k
+#brew install ccze
 
 # Google
 brew install --cask google-drive
 brew install --cask google-chrome
 # Editor
-brew install --cask obsidian
+brew install --cask scrivener
 brew install --cask visual-studio-code
+
 brew install --cask notion
 # other
 brew install --cask iterm2
 brew install --cask alfred
 brew install --cask clipy
+brew install --cask popclip
+brew install --cask dropbox
+brew install --cask bettertouchtool
+brew install --cask scroll-reverser
+brew install --cask cheatsheet
+brew install --cask tweeten
+brew install --cask discord
